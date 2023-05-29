5505a43cc4a6e292277a34877285b378b95972b3
GeorgiusDomna <130830163+GeorgiusDomna@users.noreply.github.com>
2023-05-30 01:26:06 +0400

    added the second project

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -1,6 +1,9 @@
 import img01 from './../img/projects/mini/01.webp';
 import imgBig01 from './../img/projects/big/01.webp';
 
+import img02 from './../img/projects/mini/02.webp';
+import imgBig02 from './../img/projects/big/02.webp';
+
 export const projects = [
   {
     titel: "Photographer's portfolio",
@@ -9,5 +12,13 @@ export const projects = [
     imgBig: imgBig01,
     gitHubLink: 'https://github.com/GeorgiusDomna/Portfolio-Photograph',
     vercelLink: 'https://portfolio-photograph.vercel.app/',
+  },
+  {
+    titel: "Weather forecast",
+    skills: 'JavaScript, React, CSS/SASS, HTML',
+    img: img02,
+    imgBig: imgBig02,
+    gitHubLink: 'https://github.com/GeorgiusDomna/weather',
+    vercelLink: 'https://weather-two-gamma.vercel.app/',
   },
 ]
